4124bbbdc5d9fddeb551ad79a9f27527e7d2f7ab
felipevinicios <felipe.1577@outlook.com>
2025-05-25 17:13:03 -0300

    Serviço e componente registro

--- src/main/java/com/educandoweb/curso/recursos/UsuarioRecurso.java
+++ src/main/java/com/educandoweb/curso/recursos/UsuarioRecurso.java
@@ -1,21 +1,39 @@
 package com.educandoweb.curso.recursos;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.curso.entities.Usuario;
+import com.educandoweb.curso.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioRecurso {
+	
+	@Autowired
+	private UsuarioService service;
 
 	@GetMapping
-	public ResponseEntity<Usuario> findAll() {
-		Usuario u = new Usuario (1L,"Felipe", "felipe.1577@outlook.com", "908050807", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>> findAll() {
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 		
 	
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+		
+	}
+	
+	
 }
--- src/main/java/com/educandoweb/curso/repositories/UsuarioRepository.java
+++ src/main/java/com/educandoweb/curso/repositories/UsuarioRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.curso.entities.Usuario;
 
+
 public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
 
 }
